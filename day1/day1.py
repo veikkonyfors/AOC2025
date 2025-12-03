fd9f68d687cdde56045d842f6ca28db9fde52759
Veikko Nyfors <veikkonyfors@gmail.com>
2025-12-03 11:12:39 +0200

    Day1 Part2 solved

--- day1/day1.py
+++ day1/day1.py
@@ -13,15 +13,27 @@ def turn(dial, lrnn):
         newdial = (dial + number) % 100
 
         return newdial
-def day1(lines,startdial):
+
+
+def day1(lines,startdial,puzz=1):
 
     dial = startdial
     zeroes = 0
 
     for lrnn in lines:
+
+        if puzz == 2:
+            fullrounds = int(lrnn[1:]) // 100
+            zeroes += fullrounds
+            remainder = int(lrnn[1:]) % 100
+            if lrnn[0] == "L" and dial != 0:
+                zeroes += 1 if dial - int(remainder) < 0 else 0
+            if lrnn[0] == "R" and dial != 0:
+                zeroes += 1 if dial + int(remainder) > 100 else 0
+
         dial = turn(dial, lrnn)
-        #print(f"{lrnn.rstrip()}, {dial}")
         if dial == 0: zeroes += 1
+        #print(f"{lrnn.rstrip()}, {dial}, {zeroes}")
 
     return zeroes
 
@@ -29,5 +41,7 @@ def day1(lines,startdial):
 if __name__ == '__main__':
     lines = common.read_input("input_test")
     print(f"{day1(lines, 50)}")
+    print(f"{day1(lines, 50, 2)}")
     lines = common.read_input("input")
     print(f"{day1(lines, 50)}")
+    print(f"{day1(lines, 50, 2)}")
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -10,3 +10,11 @@ def test_turnok():
     assert day1.turn(1, "L1") == 0
     assert day1.turn(99, "R2") == 1
 
+def test_day1():
+    assert day1.day1(["L743"], 43, 2) == 8
+    assert day1.day1(["r4"], 96, 2) == 1
+    assert day1.day1(["L5"], 0, 2) == 0
+    assert day1.day1(["L1"],1) == 1
+    assert day1.day1(["L701"], 2, 2) == 8
+
+
