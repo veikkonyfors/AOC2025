a69a64ea94bfba732d0f0a6161ecc556a5605051
Veikko Nyfors <veikkonyfors@gmail.com>
2025-12-05 12:12:24 +0200

    Day2 Part2 solved

--- day2/day2.py
+++ day2/day2.py
@@ -1,3 +1,5 @@
+import re
+
 import common
 
 
@@ -18,33 +20,75 @@ class IdRange:
         self.start = range.split('-')[0]
         self.end = range.split('-')[1]
 
-    def get_invalids(self):
+    def is_invalid1(self, i: int):
+        s = str(i)
+        mid = len(s) // 2
+        firsthalf = s[0:mid]
+        secondhalf = s[mid:]
+        if firsthalf == secondhalf: return True
+        return False
+
+    def is_invalid2(self, i: int):
+        s = str(i)
+        match = re.match(r'^(.+?)\1+$', s)
+        if match:
+            #print(f"match:{match.group(1)}")
+            return True
+
+        return False
+
+    def get_invalids1(self):
         invalids = []
 
         for i in range(int(self.start), int(self.end)+1):
-            s = str(i)
-            mid = len(s) // 2
-            firsthalf = s[0:mid]
-            secondhalf = s[mid:]
-            if firsthalf == secondhalf: invalids.append(int(s))
+            if self.is_invalid1(i): invalids.append(i)
+
+        return invalids
+
+    def get_invalids2(self):
+        invalids = []
+
+        for i in range(int(self.start), int(self.end)+1):
+            if self.is_invalid2(i): invalids.append(i)
 
         return invalids
 
     def to_string(self):
         return self.start + "-" + self.end
 
-def addup_invalids(lines):
+def addup_invalids1(lines):
     ranges = get_ranges(lines)
     addup = 0
     for range in ranges:
         idrange = IdRange(range)
-        invalids = idrange.get_invalids()
+        invalids = idrange.get_invalids1()
         addup += sum(invalids)
 
     return addup
 
+def addup_invalids2(lines):
+    ranges = get_ranges(lines)
+    addup = 0
+    for range in ranges:
+        idrange = IdRange(range)
+        invalids = idrange.get_invalids2()
+        addup += sum(invalids)
+
+    return addup
+
+def day2(lines,part=1):
+    if part == 1: return addup_invalids1(lines)
+    if part == 2: return addup_invalids2(lines)
+    return -1
+
+
 
 if __name__ == '__main__':
     lines = common.read_input("input_test")
+    print(f"{day2(lines)}")
+    print(f"{day2(lines,2)}")
+    lines = common.read_input("input")
+    print(f"{day2(lines)}")
+    print(f"{day2(lines,2)}")
 
 
--- day2/test_day2.py
+++ day2/test_day2.py
@@ -12,14 +12,25 @@ def test_get_ranges():
 def test_idrange():
     idrange = day2.IdRange("11-22")
     assert idrange.to_string() == "11-22"
-    assert idrange.get_invalids() == [11, 22]
-
+    assert idrange.get_invalids1() == [11, 22]
+    idrange = day2.IdRange("95-115")
+    assert idrange.get_invalids1() == [99]
+    assert idrange.get_invalids2() == [99, 111]
+    idrange = day2.IdRange("998-1012")
+    assert idrange.get_invalids1() == [1010]
+    assert idrange.get_invalids2() == [999, 1010]
     idrange = day2.IdRange("38593856-38593862")
-    assert idrange.get_invalids() == [38593859]
+    assert idrange.get_invalids1() == [38593859]
+    assert idrange.get_invalids2() == [38593859]
+    idrange = day2.IdRange("2121212118-2121212124")
+    assert idrange.get_invalids1() == []
+    assert idrange.get_invalids2() == [2121212121]
 
 def test_addup_invalids():
     lines = common.read_input("input_test")
-    assert day2.addup_invalids(lines) == 1227775554
+    assert day2.addup_invalids1(lines) == 1227775554
+    assert day2.addup_invalids2(lines) == 4174379265
 
     lines = common.read_input("input")
-    assert day2.addup_invalids(lines) == 29940924880
+    assert day2.addup_invalids1(lines) == 29940924880
+    assert day2.addup_invalids2(lines) == 48631958998
