0abf20e1e974b0e82d15a0b366c625c2467ea0b4
Veikko Nyfors <veikkonyfors@gmail.com>
2025-12-15 09:08:34 +0200

    Day4 Part1 solved

---
+++ day4/day4.py
@@ -0,0 +1,54 @@
+import common
+from typing import List
+
+class Grid:
+
+    def __init__(self, lines: List[str]):
+        self.grid = [list(line) for line in lines]
+
+    def to_string(self):
+        return str(self.grid)
+
+    def count_adjacent_rolls(self, row, col):
+
+        directions = [(-1, -1), (-1, 0), (-1, 1),
+                      (0, -1), (0, 1),
+                      (1, -1), (1, 0), (1, 1)]
+
+        count = 0
+
+        for dr, dc in directions:
+            new_row, new_col = row + dr, col + dc
+
+            if 0 <= new_row < len(self.grid) and 0 <= new_col < len(self.grid[0]):
+                if self.grid[new_row][new_col] == "@":
+                    count += 1
+
+        return count
+
+    def count_accessible_rolls(self):
+        count = 0
+        for ir, row in enumerate(self.grid):
+            for ic, col in enumerate(row):
+                if col == "@":
+                    adjacent_rolls = self.count_adjacent_rolls(ir, ic)
+                    if adjacent_rolls < 4:
+                        count += 1
+
+        return count
+
+def day4(input: str,part=1):
+    lines = common.read_input(input)
+    grid = Grid(lines)
+    if part == 1: return grid.count_accessible_rolls()
+    if part == 2: return 1
+
+    return -1
+
+if __name__ == '__main__':
+    print(f"{day4('input_test')}")
+    #print(f"{day4('input_test', 2)}")
+    print(f"{day4('input')}")
+    #print(f"{day4('input', 2)}")
+
+
---
+++ day4/test_day4.py
@@ -0,0 +1,28 @@
+import pytest
+import common
+import day4
+
+def test_grid():
+    lines = common.read_input("input_test")
+    grid = day4.Grid(lines)
+    assert grid.to_string() == \
+        "[['.', '.', '@', '@', '.', '@', '@', '@', '@', '.', '\\n'], ['@', '@', '@', '.', '@', '.', '@', '.', '@', "\
+        "'@', '\\n'], ['@', '@', '@', '@', '@', '.', '@', '.', '@', '@', '\\n'], ['@', '.', '@', '@', '@', '@', '.', "\
+        "'.', '@', '.', '\\n'], ['@', '@', '.', '@', '@', '@', '@', '.', '@', '@', '\\n'], ['.', '@', '@', '@', '@', "\
+        "'@', '@', '@', '.', '@', '\\n'], ['.', '@', '.', '@', '.', '@', '.', '@', '@', '@', '\\n'], ['@', '.', '@', "\
+        "'@', '@', '.', '@', '@', '@', '@', '\\n'], ['.', '@', '@', '@', '@', '@', '@', '@', '@', '.', '\\n'], ['@', "\
+        "'.', '@', '.', '@', '@', '@', '.', '@', '.', '\\n']]"
+    assert grid.count_adjacent_rolls(0,0) == 2
+    assert grid.count_adjacent_rolls(1, 1) == 6
+    assert grid.count_adjacent_rolls(9, 9) == 2
+    assert  grid.count_accessible_rolls() == 13
+
+    lines = common.read_input("input")
+    grid = day4.Grid(lines)
+    assert grid.count_accessible_rolls() == 1409
+    
+def test_day4():
+    assert day4.day4("input_test") == 13
+    #assert day4.day4("input_test", 2) == 3121910778619
+    assert day4.day4("input") == 1409
+    #assert day4.day4("input", 2) == 173848577117276
